fe75016504788687cc4b269b48913e87bfadc434
Asyl <duldievasylbek@gmail.com>
2024-10-11 23:29:16 +0500

    EndComp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,11 @@ const About = () => {
 
                 {/* About content */}
                 <div className='md:w-2/6'>
-
+                    <h2 className='md:text-4xl text-3xl font-bold text-primary mb-5 leading-normal'>We have 
+                    been improving our product <span className='text-secondary'>for many years.</span></h2>
+                    <p className='text-tartiary text-lg mb-7'>A good example of a paragraph contains a topic conclusion.
+                    'There are many different kinds of animals that live in China.</p>
+                    <button className='btnPrimary'>Get Started</button>
                 </div>
             </div>
 
@@ -31,6 +35,7 @@ const About = () => {
                     <button className='btnPrimary'>Get Started</button>
                 </div>
             </div>
+
         </div>
     )
 };
